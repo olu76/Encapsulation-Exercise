09fa1dd7c465b57344e77e72936fbb0e3d03cfa3
olumide azeez afolabi <midas44000@yahoo.com>
2021-11-05 12:36:04 -0500

    initial commit

---
+++ BankEncapsulation Console/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation_Console
+{
+    class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Getting a deposit of {amount,0} to your account!!!");
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+        
+  
+    }
+}
---
+++ BankEncapsulation Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var BankAccount = new BankAccount();
+
+            Console.WriteLine("How much money do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            BankAccount.Deposit(amountToDeposit);
+
+            double userBalance = BankAccount.GetBalance();
+
+            Console.WriteLine($"Your current balance is {amountToDeposit,0}");
+                
+        }
+    }
+}
